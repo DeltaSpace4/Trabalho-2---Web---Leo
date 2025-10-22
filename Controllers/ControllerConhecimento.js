103a856bbe51e3fbbcfc6d187438d91cc5571d20
LeoKondii <conde.leo.feitosa@gmail.com>
2025-10-22 20:34:07 -0300

    Controllers, faltando atualizar Paths para novos nomes

---
+++ Controllers/ControllerConhecimento.js
@@ -0,0 +1,40 @@
+const db = require('../Config/Db');
+const path = require('path');
+
+module.exports = {
+    // Create
+    async getCreate(req, res) {
+        res.render('tag/tagCreate');
+    },
+    async postCreate(req, res) {
+        db.Tag.create(req.body).then(() => {
+            res.redirect('/home');
+        }).catch((err) => { console.log(err); });
+    },
+
+    // List
+    async getList(req, res) {
+        db.Tag.findAll().then(tag => {
+            res.render('tag/tagList', { tag: tag.map(TagPar => TagPar.toJSON()) });
+        }).catch((err) => { console.log(err); });
+    },
+
+    //Update
+    async getUpdate(req, res) {
+        await db.Tag.findByPk(req.params.id).then(
+            tag => res.render('tag/tagUpdate', {tag: tag.dataValues })
+        ).catch(function (err) { console.log(err); });
+    },
+    async postUpdate(req, res) {
+        await db.Tag.update(req.body, { where: { id: req.body.id } }).then(
+            res.render('home')
+        ).catch(function (err) { console.log(err); });
+    },
+
+    //Delete
+    async getDelete(req, res) {
+        await db.Tag.destroy({ where: { id: req.params.id } }).then(
+            res.render('home')
+        ).catch(err => { console.log(err); });
+    }
+}   
